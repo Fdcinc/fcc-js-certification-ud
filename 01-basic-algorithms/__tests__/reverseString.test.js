afe5f6b6b53d1405947fc150192ff3918a4e53fd
fdcinc <nickroutsis@gmail.com>
2025-08-24 12:36:38 +0300

    Fix test folder name to __tests__ and organize test files

---
+++ 01-basic-algorithms/__tests__/reverseString.test.js
@@ -0,0 +1,6 @@
+'use strict';
+const reverseString = require('../01-basic-algorithms/reverseString.js');
+
+test('reverseString is a function', () => {
+    expect(typeof reverseString).toBe('function');
+});
--- 01-basic-algorithms/reverseString.js
+++ 01-basic-algorithms/reverseString.js
@@ -1,1 +1,4 @@
-
+'use strict';
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
